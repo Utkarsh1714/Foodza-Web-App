9495e969a964cf6185caf160dc8b42fc4d91c2b3
Utkarsh1714 <utkarshpalav17@gmail.com>
2024-09-11 13:44:06 +0530

    update env and vercel.json

--- dbConnection/index.js
+++ dbConnection/index.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async (req, res) => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to Database !");
   } catch (err) {
     console.error(err.message);
